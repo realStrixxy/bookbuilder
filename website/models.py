4743412a7cc0e99fb9d7ffac3f14650d1810a224
realStrixxy <47069036+realStrixxy@users.noreply.github.com>
2024-06-11 16:15:05 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -5,18 +5,18 @@ from sqlalchemy.sql import func
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    title = db.Column(db.String(1000))
-    data = db.Column(db.Text)
+    title = db.Column(db.String)
+    data = db.Column(db.String)
     createdDate = db.Column(db.DateTime(timezone=True), default=func.now())
-    misc = db.Column(db.Text)
+    misc = db.Column(db.String)
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    username = db.Column(db.String(25), unique=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.Text)
+    name = db.Column(db.String)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String(250))
     joinedDate = db.Column(db.DateTime(timezone=True), default=func.now())
     book = db.relationship('Book')
-    misc = db.Column(db.Text)
+    misc = db.Column(db.String)
