151ab5733b083bc0e96078b00243f4be18cb08b6
realStrixxy <47069036+realStrixxy@users.noreply.github.com>
2024-06-11 16:25:05 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -5,18 +5,18 @@ from sqlalchemy.sql import func
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    title = db.Column(db.String)
-    data = db.Column(db.String)
+    title = db.Column(db.Text)
+    data = db.Column(db.Text)
     createdDate = db.Column(db.DateTime(timezone=True), default=func.now())
-    misc = db.Column(db.String)
+    misc = db.Column(db.Text)
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(100))
     username = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String(250))
+    email = db.Column(db.Text, unique=True)
+    password = db.Column(db.Text)
     joinedDate = db.Column(db.DateTime(timezone=True), default=func.now())
     book = db.relationship('Book')
-    misc = db.Column(db.String)
+    misc = db.Column(db.Text)
