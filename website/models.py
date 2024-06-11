1f482177ef1a5ed598132fa395496b1897855e9a
realStrixxy <47069036+realStrixxy@users.noreply.github.com>
2024-06-11 15:46:04 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(100))
     username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(1000))
     joinedDate = db.Column(db.DateTime(timezone=True), default=func.now())
     book = db.relationship('Book')
     misc = db.Column(db.String(1000000))
